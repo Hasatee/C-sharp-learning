770e6895eac6d5fbc608bc926a4126bdf8d416f0
Hasatee <143634039+Hasatee@users.noreply.github.com>
2025-08-25 16:55:18 +0700

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,17 @@
+﻿// nhập vào 1 chuỗi và cho biết
+// có bao nhiêu ký tự hoa, thường, số
+// có bao nhiêu khoảng trắng
+
+Console.Write("Nhập chuỗi s = ");
+int upper = 0, lower = 0, number = 0, space = 0;
+string s = Console.ReadLine();
+foreach (char c in s){
+    if (char.IsUpper(c)) upper++;
+    else if (char.IsLower(c)) lower++;
+    else if (char.IsDigit(c)) number++;
+    else if (char.IsWhiteSpace(c)) space++;
+    }
+Console.WriteLine("Số ký tự hoa: " + upper);
+Console.WriteLine("Số ký tự thường: " + lower);
+Console.WriteLine("Số ký tự số: " + number);
+Console.WriteLine("Số khoảng trắng: " + space);
